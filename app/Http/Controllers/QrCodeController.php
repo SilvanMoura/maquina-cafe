5b31c0e8dc010c7ef67a8a38a338be2a76a2a6ea
Silvan Moura <silvancortesmoura@outlook.com>
2024-12-29 13:50:58 -0300

    Creation of QrCodeController methods

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PagSeguroService;
 use Illuminate\Http\Request;
 
 class QrCodeController extends Controller
 {
-    //
+    private $pagSeguroService;
+
+    public function __construct(PagSeguroService $pagSeguroService)
+    {
+        $this->pagSeguroService = $pagSeguroService;
+    }
+
+    public function generate(Request $request)
+    {
+        $referenceId = $request->input('machine_id');
+        $qrCodeData = $this->pagSeguroService->generateQrCode($referenceId);
+
+        return response()->json($qrCodeData);
+    }
+
+    public function transactionDetails(Request $request, $transactionId)
+    {
+        $details = $this->pagSeguroService->getTransactionDetails($transactionId);
+
+        return response()->json($details);
+    }
 }
