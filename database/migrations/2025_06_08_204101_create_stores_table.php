21981a405262493cc9ab17c7241d14c43bf6efda
SilvanMoura <silvancortesmoura@outlook.com>
2025-06-10 21:52:20 -0300

    Creating migration and model for store

---
+++ database/migrations/2025_06_08_204101_create_stores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpfcnpj')->nullable();
+            $table->string('nameStore')->nullable();
+            $table->string('endereco')->nullable();
+            $table->string('complemento')->nullable();
+            $table->string('bairro')->nullable();
+            $table->string('cep')->nullable();
+            $table->string('cidade')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stores');
+    }
+};
