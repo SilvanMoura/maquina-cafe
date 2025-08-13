0a95f0fcf7d7efb3de58a07ac69b0021ff86eb00
SilvanMoura <silvancortesmoura@outlook.com>
2025-08-12 23:01:28 -0300

    Installing Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use BeyondCode\LaravelWebSockets\Facades\WebSocket;
-use App\Http\Controllers\QrCodeController;
-use App\Http\Controllers\StoreController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ModuleController;
-use App\Http\Controllers\NotificationController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -20,32 +14,18 @@ use App\Http\Controllers\NotificationController;
 |
 */
 
-Route::get('/dashboard', [DashboardController::class, 'dashboardView']);
-
-Route::get('/lojas', [StoreController::class, 'getStoreData']); //obtem todas as lojas
-Route::get('/lojas/adicionar', [StoreController::class, 'newStoreView']);
-Route::post('/lojas/adicionar', [StoreController::class, 'newStore']);
-Route::get('/pos', [StoreController::class, 'getPosData']); //obtem todos os pontos de venda/caixa (pos)
-Route::get('/vendas', [StoreController::class, 'salesView']);
-
-Route::get('/modulos', [ModuleController::class, 'modulesView']); 
-Route::get('/modulos/online', [ModuleController::class, 'modulesOnlineView']); 
-Route::get('/modulos/adicionar', [ModuleController::class, 'newModuleView']);
-Route::post('/modulos/adicionar', [ModuleController::class, 'newModule']);
-Route::get('/cupons', [ModuleController::class, 'couponsView']);
-Route::get('/cupons/adicionar', [ModuleController::class, 'newCouponView']);
-Route::post('/cupons/adicionar', [ModuleController::class, 'newCoupon']);
-Route::get('/readCode', [ModuleController::class, 'readCodeView']);
-Route::post('/readCode', [ModuleController::class, 'depositCoupon']);
-Route::get('/controle', [ModuleController::class, 'controlRemoteView']);
-Route::post('/sendCommand', [ModuleController::class, 'sendCommandModule']);
-
-//envia o credito do cupom para o modulo 
-
-Route::get('/generateQrCode', [QrCodeController::class, 'qrCodeView']);
-Route::get('/websocket', function () {
-    return view('websocket');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::post('/qrcode', [QrCodeController::class, 'generate']);
-Route::post('/notifications', [NotificationController::class, 'handle']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
